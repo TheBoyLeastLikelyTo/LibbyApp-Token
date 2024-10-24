7087ab1f35e233391b4f6cf0bbc55a3d72681cf4
simon-techkid <simon.techkid@outlook.com>
2024-10-24 07:53:03 -0700

    Version 1.2

--- popup.js
+++ popup.js
@@ -2,32 +2,57 @@
 
 document.addEventListener('DOMContentLoaded', function () {
   const tokenInput = document.getElementById('token');
-  const errorDiv = document.getElementById('error');
-  const buttonContainer = document.getElementById('button-container');
+  const messageDiv = document.getElementById('message');
+  const buttonContainer = document.getElementById('container-info');
+  const containerHidden = 'none';
+  const containerShown = 'block';
+  const emptyToken = '';
+  const reloadTimer = 1000; // Increase for slower connections
 
-  // Function to display error messages
-  function showError(message) {
-    tokenInput.value = '';
-    buttonContainer.style.display = 'none'; // Hide buttons
-    errorDiv.textContent = message;
+  // Change the button hidden/shown state
+  function containerChangeButtonState(show) {
+    if (show == false) {
+      buttonContainer.style.display = containerHidden; // Hide buttons
+    }
+    else {
+      buttonContainer.style.display = containerShown; // Show buttons
+    }
   }
 
+  // Function to display error messages
+  function showMessage(message, type) {
+    
+    if (type == 'error') {
+      messageDiv.style.color = 'red';
+    } else {
+      messageDiv.style.color = 'black';
+    }
+    
+    tokenInput.value = emptyToken;
+    messageDiv.textContent = message;
+  }
+
+  // Function to show the token
+  function showToken(data) {
+    if (data.bearerToken) {
+      tokenInput.value = data.bearerToken;
+      messageDiv.textContent = emptyToken;
+      containerChangeButtonState(true);
+    } else {
+      showMessage("No token found. Please sign in and try refreshing.", "error");
+      containerChangeButtonState(false);
+    }
+  }
+  
   // Check if the user is on libbyapp.com
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     const currentTab = tabs[0];
     if (!currentTab.url.includes("libbyapp.com")) {
-      showError("Please open libbyapp.com to use this extension.");
+      showMessage("Please open libbyapp.com to use this extension.", "error");
+      containerChangeButtonState(false);
     } else {
       // Display the bearer token in the text box
-      chrome.storage.local.get('bearerToken', (data) => {
-        if (data.bearerToken) {
-          tokenInput.value = data.bearerToken;
-          errorDiv.textContent = '';
-          buttonContainer.style.display = 'block'; // Show buttons
-        } else {
-          showError("No token found. Please sign in and try refreshing.");
-        }
-      });
+      chrome.storage.local.get('bearerToken', (data) => showToken(data));
     }
   });
 
@@ -35,17 +60,11 @@ document.addEventListener('DOMContentLoaded', function () {
   document.getElementById('refresh').addEventListener('click', () => {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       chrome.tabs.reload(tabs[0].id);
+      showMessage("Refreshing Token!");
+      //containerChangeButtonState(false);
       setTimeout(() => {
-        chrome.storage.local.get('bearerToken', (data) => {
-          if (data.bearerToken) {
-            tokenInput.value = data.bearerToken;
-            errorDiv.textContent = '';
-            buttonContainer.style.display = 'block'; // Show buttons
-          } else {
-            showError("No token found. Please sign in and try refreshing.");
-          }
-        });
-      }, 2000); // wait for the reload to complete
+        chrome.storage.local.get('bearerToken', (data) => showToken(data));
+      }, reloadTimer); // wait for the reload to complete
     });
   });
 
